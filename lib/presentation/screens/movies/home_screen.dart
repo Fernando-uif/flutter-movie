e29453c389f0173d9a582376bf73961efa4d6825
Fernando sosa <fsosat1400@alumno.ipn.mx>
2024-07-01 20:31:13 -0600

    add movies

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,8 @@
 // import 'package:cinemapedia/config/constans/environment.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_privoders.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,6 +10,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Placeholder());
+    return const _HomeView();
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    if (nowPlayingMovies.isEmpty) return const CircularProgressIndicator();
+    return Scaffold(
+        body: ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    ));
   }
 }
